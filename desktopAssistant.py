0f237dc0b2f6efc9766f8ce796cde0b3670f2cce
anuragjambhulkar <gameratx5@gmail.com>
2024-01-18 23:13:14 +0530

    version

--- desktopAssistant.py
+++ desktopAssistant.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
+matplotlib inline
 
 import chart_studio.plotly as py
 import plotly.graph_objects as go
